b24a2d0540e1055cf63ecc197882a4ede4554164
kimmandarin <gyu6491@naver.com>
2022-05-16 22:50:57 +0900

    20220516

--- src/main/java/com/example/foif/service/MemberService.java
+++ src/main/java/com/example/foif/service/MemberService.java
@@ -1,17 +1,13 @@
 package com.example.foif.service;
 
-import com.example.foif.domain.Member;
 import com.example.foif.domain.MemberDTO;
-import com.example.foif.domain.Role;
 import com.example.foif.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -38,17 +34,11 @@ public class MemberService {
         User user = (User)principal;
         String username = ((User) principal).getUsername();
 
-        List<MemberDTO> resultList = em.createQuery("SELECT m.email, m.policeStation, m.phoneNumber, m.policeId FROM MemberDTO m where m.policeId = :username")
+        List<MemberDTO> resultList = em.createQuery("SELECT m FROM MemberDTO m where m.policeId = :username", MemberDTO.class)
                 .setParameter("username", username)
                 .getResultList();
 
-        MemberDTO memberDTO = null;
-        memberDTO.setEmail(String.valueOf(resultList.get(0).getEmail()));
-        memberDTO.setPoliceId(String.valueOf(resultList.get(0).getPoliceId()));
-        memberDTO.setPhoneNumber(String.valueOf(resultList.get(0).getPhoneNumber()));
-        memberDTO.setPoliceStation(String.valueOf(resultList.get(0).getPoliceStation()));
-
-        return memberDTO;
+        return resultList.get(0);
     }
 
     public void findPassword(String username){
